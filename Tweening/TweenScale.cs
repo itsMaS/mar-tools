0ef0addb930e1f4d1922e00260c5fbe8167f94f7
Martynas Šiškauskas <martynas.siskauskas@sneakybox.biz>
2025-02-04 12:22:12 +0200

    Allow to shake scale along 3D axis

--- Tweening/TweenScale.cs
+++ Tweening/TweenScale.cs
@@ -6,12 +6,25 @@ namespace MarTools
     
     public class TweenScale : TweenCore
     {
+        public bool use3D = false;
+
         public float startingScale = 1;
         public float endingScale = 1.1f;
-    
+
+        public Vector3 startingScale3D = Vector3.one;
+        public Vector3 endingScale3D = Vector3.one;
+
         public override void SetPose(float t)
         {
-            transform.localScale = Vector3.one * Mathf.LerpUnclamped(startingScale, endingScale, t);
+            if(use3D)
+            {
+                transform.localScale = Vector3.Lerp(startingScale3D, endingScale3D, t);
+            }
+            else
+            {
+                transform.localScale = Vector3.one * Mathf.LerpUnclamped(startingScale, endingScale, t);
+            }
+
         }
     }
     
